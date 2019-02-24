d4105761e21c67c298435d5b443cf0ef835623be
Broivula <elias.koivula1@hotmail.com>
2019-02-24 17:23:57 +0200

    socket io basing and other stuff

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 'use strict';
+require('dotenv').config()
 const express = require('express');
 const multer = require('multer');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const upload = multer({dest: 'uploads/'});
+
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'elias_rasp_pi',
-    password: 'ripirapiDF20PI',
-    database: 'shopping_list_app'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB
 });
 
 const app = express();
+const http = require('http').Server(app);
+const server = app.listen(2222 ,() => {console.log('server running on port 2222')});
+const io = require('socket.io').listen(server);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+io.on('connection', (socket) => {
+    socket.on('message', (message) => {
+        console.log('kikkel');
+    });
+    console.log('a user has connected.');
+});
 
 db.connect((err) => {
     if(err){
@@ -29,31 +41,31 @@ app.get('/list', (req, res) => {
     db.query(query, (err, result) => {
         if(err) throw err;
         res.send(result);
-      console.log(result);
     })
 });
 
 app.post('/post/item', (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     let item = req.body.item;
     let user = req.body.user;
     db.query( 'INSERT IGNORE INTO list (item, user, date) VALUES (?, ?, CURRENT_TIMESTAMP());', [item, user],  (err, result) => {
         if(err) throw err;
-        console.log(result);
+       // console.log(result);
+        io.emit('message', req.body);
         res.json(req.body).status(200);
     });
 });
 
 app.delete('/delete/item', (req, res) => {
+    console.log(req.body);
    let item = req.body.item;
    console.log('poistettava esine: ' + item);
    db.query('DELETE FROM list WHERE item=?', [item], (err, result) => {
        if(err) throw err;
-       console.log(result);
+      // console.log(result);
        res.json(req.body).status(200);
    })
 });
 
 
 
-app.listen(2222 ,() => {console.log('server running on port 2222')});
